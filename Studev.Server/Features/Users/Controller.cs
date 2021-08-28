fc7fdabd229a9e20b4c97b5469536a248d19b4ea
netotz <neto.otz@hotmail.com>
2021-08-28 17:50:29 -0500

    Add Users controller

---
+++ Studev.Server/Features/Users/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Studev.Server.Features.Users {
+    [Route("api/users")]
+    [ApiController]
+    [AllowAnonymous]
+    public class Controller : ControllerBase {
+        private readonly IMediator _mediator;
+
+        public Controller(IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> SignUp([FromBody] Add.Command request) {
+            return Ok(await _mediator.Send(request));
+        }
+    }
+}
