463153d380ee5b4501b065d333021432cb500999
netotz <neto.otz@hotmail.com>
2021-08-28 15:00:13 -0500

    Add database context

---
+++ Studev.Server/Database/StudevContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using Studev.Server.Models;
+
+namespace Studev.Server.Database {
+    public class StudevContext : DbContext {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Recruiter> Recruiters { get; set; }
+
+        public StudevContext(DbContextOptions<StudevContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Student>(student => {
+                student.ToTable(nameof(Student));
+            });
+
+            modelBuilder.Entity<Recruiter>(recruiter => {
+                recruiter.ToTable(nameof(Recruiter));
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
