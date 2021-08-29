6275ed67e8ba920184eb75ca61a9f3b9ff075a3f
netotz <neto.otz@hotmail.com>
2021-08-29 02:08:01 -0500

    Comment methods as they are ambiguous

--- Studev.Server/Features/Users/Controller.cs
+++ Studev.Server/Features/Users/Controller.cs
@@ -15,7 +15,6 @@ namespace Studev.Server.Features.Users {
     [AllowAnonymous]
     public class Controller : ControllerBase {
         private readonly IMediator _mediator;
-        private ApiService apiService;
 
         public Controller(IMediator mediator) {
             _mediator = mediator;
@@ -45,24 +44,24 @@ namespace Studev.Server.Features.Users {
         }
 
         //gets profile of user's github username
-        public async Task<JObject> GetProfile(string username)
-        {
-            var profile = await apiService.GetObject($"users/{username}");
-            return profile;
-        }
+        //public async Task<JObject> GetProfile(string username)
+        //{
+        //    var profile = await apiService.GetObject($"users/{username}");
+        //    return profile;
+        //}
 
-        //get repos of specified user's github username
-        public async Task<JArray> GetUserRepos(string username)
-        {
-            var repos = await apiService.GetArray($"users/{username}/repos?type=owner");
-            return repos;
-        }
+        ////get repos of specified user's github username
+        //public async Task<JArray> GetUserRepos(string username)
+        //{
+        //    var repos = await apiService.GetArray($"users/{username}/repos?type=owner");
+        //    return repos;
+        //}
 
-        //get repo languages given a github username and repo name
-        public async Task<JObject> GetRepoLanguages(string username, string reponame)
-        {
-            var repos = await apiService.GetObject($"repos/{username}/{reponame}/languages");
-            return repos;
-        }
+        ////get repo languages given a github username and repo name
+        //public async Task<JObject> GetRepoLanguages(string username, string reponame)
+        //{
+        //    var repos = await apiService.GetObject($"repos/{username}/{reponame}/languages");
+        //    return repos;
+        //}
     }
 }
