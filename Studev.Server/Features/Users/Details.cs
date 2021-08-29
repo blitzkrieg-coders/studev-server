bec8a193d8cc1e915d1633742ae547c260d89314
netotz <neto.otz@hotmail.com>
2021-08-29 07:42:32 -0500

    Add properties to StudentData

--- Studev.Server/Features/Users/Details.cs
+++ Studev.Server/Features/Users/Details.cs
@@ -22,6 +22,10 @@ namespace Studev.Server.Features.Users {
             }
 
             public string GitHubLogin { get; init; }
+            public string Name { get; init; }
+            public string AvatarUrl { get; init; }
+            public string Biography { get; init; }
+            public string Location { get; init; }
             public DateTime StudyStart { get; init; }
             public DateTime StudyEnd { get; init; }
             public string Career { get; init; }
@@ -47,21 +51,29 @@ namespace Studev.Server.Features.Users {
             }
 
             public async Task<StudentData> Handle(Query request, CancellationToken cancellationToken) {
+                var user = await _apiService.GetObject($"https://api.github.com/users/{request.GitHubLogin}");
+                if (user is null) {
+                    return null;
+                }
                 var studentData = await _context.Students
                     .Where(s => s.GitHubLogin == request.GitHubLogin)
                     .Select(s => new StudentData {
                         GitHubLogin = s.GitHubLogin,
+                        Name = user["name"].ToString(),
+                        AvatarUrl = user["avatar_url"].ToString(),
+                        Biography = user["bio"].ToString(),
+                        Location = user["location"].ToString(),
                         StudyStart = s.StudyStart,
                         StudyEnd = s.StudyEnd,
                         Career = s.Career,
                         School = s.School
                     })
-                    .SingleOrDefaultAsync();
+                    .SingleOrDefaultAsync(cancellationToken);
                 if (studentData is null) {
                     return null;
                 }
 
-                var repos = await _apiService.GetArray($"https://api.github.com/users/{request.GitHubLogin}/repos?type=owner");
+                var repos = await _apiService.GetArray(user["repos_url"].ToString());
                 var studentRepos = new List<StudentData.RepositoryStats>();
                 foreach (var repo in repos.Where(r => !(bool)r["fork"])) {
                     var contributors = await _apiService.GetArray(repo["contributors_url"].ToString());
