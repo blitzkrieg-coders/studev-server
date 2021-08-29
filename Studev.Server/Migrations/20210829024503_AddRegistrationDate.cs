a26717acd330b6512c4f59f384f21a3d4ebeeb53
netotz <neto.otz@hotmail.com>
2021-08-28 21:46:27 -0500

    Add RegistrationDate property

---
+++ Studev.Server/Migrations/20210829024503_AddRegistrationDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Studev.Server.Migrations
+{
+    public partial class AddRegistrationDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationDate",
+                table: "Student",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(2021, 8, 28, 21, 44, 59, 813, DateTimeKind.Local).AddTicks(2844));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationDate",
+                table: "Student");
+        }
+    }
+}
