75fcbb5d37e6ecd3455aad153c697bd89a62e689
Diegobr21 <jdiegobrana@gmail.com>
2021-08-28 20:53:27 -0500

    HttpClient as service and Service class

---
+++ Studev.Server/Services/ApiRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studev.Server.Services
+{
+    public class ApiRequest
+    {
+    }
+}
--- Studev.Server/Startup.cs
+++ Studev.Server/Startup.cs
@@ -26,6 +26,7 @@ namespace Studev.Server {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddHttpClient();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Studev.Server", Version = "v1" });
             });
