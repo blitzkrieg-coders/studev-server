eec6dd796ff9f78aac688fa2b2da8661f287d6cb
netotz <neto.otz@hotmail.com>
2021-08-29 07:17:53 -0500

    Add Authorization header to ApiService

--- Studev.Server/Services/ApiService.cs
+++ Studev.Server/Services/ApiService.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
 
 using Newtonsoft.Json.Linq;
 
@@ -10,33 +11,33 @@ namespace Studev.Server.Services {
     {
         public HttpClient Client { get; }
 
-        public ApiService(HttpClient client)
-        {
-            client.BaseAddress = new Uri("https://api.github.com/");
+        public ApiService(HttpClient client, IConfiguration configuration) {
             // GitHub requires a user-agent
             client.DefaultRequestHeaders.UserAgent
-                .Add(new ProductInfoHeaderValue("Studev", "0.0.0"));
+                .Add(new ProductInfoHeaderValue("Studev", "1.0.0"));
+            client.DefaultRequestHeaders
+                .Add("Authorization", $"token {configuration.GetValue<string>("GitHubToken")}");
 
             Client = client;
         }
 
         public async Task<JArray> GetArray(string url)
         {
-            var response = await Client.GetAsync($"{Client.BaseAddress}{url}");
+            var response = await Client.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
             return JArray.Parse(content);
         }
 
         public async Task<JObject> GetObject(string url)
         {
-            var response = await Client.GetAsync($"{Client.BaseAddress}{url}");
+            var response = await Client.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
             return JObject.Parse(content);
         }
 
         public async Task<string> GetContent(string url)
         {
-            var response = await Client.GetAsync($"{Client.BaseAddress}{url}");
+            var response = await Client.GetAsync(url);
             return await response.Content.ReadAsStringAsync();
         }
     }
